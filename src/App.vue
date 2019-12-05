a9aa02ef2f1624339e9e337a6bdd1c7b43e038a6
fr <400822@163.com>
2019-12-06 00:39:34 +0800

    图形验证码完善

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,21 @@
 				<div class="layui-form-item">
 					<label class="layui-form-label">用户名</label>
 					<div class="layui-input-block">
-						<input type="text" name="title" required lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
+						<input type="text" name="title" required lay-verify="required" placeholder="请输入用户名" autocomplete="off" class="layui-input">
 					</div>
 				</div>
 				<div class="layui-form-item">
 					<label class="layui-form-label">密码</label>
 					<div class="layui-input-block">
-						<input type="password" name="title" required lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
+						<input type="password" name="title" required lay-verify="required" placeholder="请输入密码" autocomplete="off" class="layui-input">
 					</div>
 				</div>
 				<div class="layui-form-item">
 					<label class="layui-form-label">验证码</label>
 					<div class="layui-input-inline">
-						<input autocomplete="off" class="layui-input" lay-verify="required" name="title" placeholder="请输入标题" required type="text">
+						<input autocomplete="off" class="layui-input" lay-verify="required" name="title" placeholder="请输入验证码" required type="text">
 					</div>
-					<div class="layui-form-mid layui-word-aux" v-html="svg"></div>
+					<div class="layui-form-mid layui-word-aux svg" @click="getCaptcha" v-html="svg"></div>
 				</div>
 			</form>
 			<button type="button" class="layui-btn">点击登陆</button>
@@ -37,12 +37,16 @@
 				svg: ''
 			}
 		},
-		async mounted() {
-			let captcha = await axios.get('http://localhost:3000/getCaptcha')
-			if (captcha.status == 200) {
-				if (captcha.data && captcha.data.code == 200) {
-					this.svg = captcha.data.data
-				}
+		mounted() {
+			this.getCaptcha()
+		},
+		methods: {
+			async getCaptcha() {
+				let captcha = await axios.get('http://localhost:3000/getCaptcha')
+				if (captcha.status != 200) return;
+				if (!captcha.data || captcha.data.code != 200) return;
+
+				this.svg = captcha.data.data
 			}
 		}
 	}
@@ -62,6 +66,11 @@
 		&:hover {
 			color: #009688;
 		}
+	}
+	
+	.svg {
+		position: relative;
+		top: -15px;
 	}
 	
 	input {
