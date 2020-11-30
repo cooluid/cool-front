7aec32c6ba086c644d6f0606021103712689dcdb
cooluid <173000@qq.com>
2020-11-30 19:47:15 +0800

    veevalidate修改

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -15,109 +15,119 @@
 					id="LAY_ucm"
 					style="padding: 20px 0;"
 				>
-					<div class="layui-tab-item layui-show">
-						<div class="layui-form layui-form-pane">
-							<form method="post">
-								<validation-provider name="email" rules="required|email" v-slot="{ errors }">
-									<div class="layui-form-item">
-										<label class="layui-form-label">用户名</label>
-										<div class="layui-input-inline">
-											<input
-												type="text"
-												name="username"
-												placeholder="请输入用户名"
-												v-model="userName"
-												lay-verify="required"
-												autocomplete="off"
-												class="layui-input"
-											/>
-										</div>
-										<div class="layui-form-mid">
-											<span style="color: #c00;">{{ errors[0] }}</span>
-										</div>
-									</div>
-								</validation-provider>
-								<validation-provider name="password" rules="required|min:6" v-slot="{ errors }">
-									<div class="layui-form-item">
-										<label class="layui-form-label">密码</label>
-										<div class="layui-input-inline">
-											<input
-												type="password"
-												name="password"
-												placeholder="请输入密码"
-												v-model="password"
-												lay-verify="required"
-												autocomplete="off"
-												class="layui-input"
-											/>
-										</div>
-										<div class="layui-form-mid">
-											<span style="color: #c00;">{{ errors[0] }}</span>
-										</div>
-									</div>
-								</validation-provider>
-								<div class="layui-form-item">
+					<validation-observer ref="observer" v-slot="{ validate }">
+						<div class="layui-tab-item layui-show">
+							<div class="layui-form layui-form-pane">
+								<form method="post">
 									<validation-provider
-										rules="required|length:4"
+										name="email"
+										rules="required|email"
 										v-slot="{ errors }"
-										name="code"
 									>
-										<div class="layui-row">
-											<label class="layui-form-label">验证码</label>
+										<div class="layui-form-item">
+											<label class="layui-form-label">用户名</label>
 											<div class="layui-input-inline">
 												<input
 													type="text"
-													name="code"
-													v-model="code"
+													name="username"
+													placeholder="请输入用户名"
+													v-model="userName"
 													lay-verify="required"
-													placeholder="请输入验证码"
 													autocomplete="off"
 													class="layui-input"
 												/>
-											</div>
-											<div>
-												<span class="svg" v-html="svg" @click="getCaptcha" />
 											</div>
 											<div class="layui-form-mid">
 												<span style="color: #c00;">{{ errors[0] }}</span>
 											</div>
 										</div>
 									</validation-provider>
-								</div>
-								<div class="layui-form-item">
-									<button
-										class="layui-btn"
-										lay-filter="*"
-										lay-submit
-										type="button"
-										@click="submit()"
+									<validation-provider
+										name="password"
+										rules="required|min:6"
+										v-slot="{ errors }"
 									>
-										立即登录
-									</button>
-									<span style="padding-left:20px;">
-										<router-link :to="{ name: 'forget' }"
-											>忘记密码？</router-link
+										<div class="layui-form-item">
+											<label class="layui-form-label">密码</label>
+											<div class="layui-input-inline">
+												<input
+													type="password"
+													name="password"
+													placeholder="请输入密码"
+													v-model="password"
+													lay-verify="required"
+													autocomplete="off"
+													class="layui-input"
+												/>
+											</div>
+											<div class="layui-form-mid">
+												<span style="color: #c00;">{{ errors[0] }}</span>
+											</div>
+										</div>
+									</validation-provider>
+									<div class="layui-form-item">
+										<validation-provider
+											rules="required|length:4"
+											v-slot="{ errors }"
+											name="code"
 										>
-									</span>
-								</div>
-								<div class="layui-form-item fly-form-app">
-									<span>或者使用社交账号登入</span>
-									<a
-										href
-										onclick="layer.msg('正在通过QQ登入', {icon:16, shade: 0.1, time:0})"
-										class="iconfont icon-qq"
-										title="QQ登入"
-									></a>
-									<a
-										href
-										onclick="layer.msg('正在通过微博登入', {icon:16, shade: 0.1, time:0})"
-										class="iconfont icon-weibo"
-										title="微博登入"
-									></a>
-								</div>
-							</form>
+											<div class="layui-row">
+												<label class="layui-form-label">验证码</label>
+												<div class="layui-input-inline">
+													<input
+														type="text"
+														name="code"
+														v-model="code"
+														lay-verify="required"
+														placeholder="请输入验证码"
+														autocomplete="off"
+														class="layui-input"
+													/>
+												</div>
+												<div>
+													<span class="svg" v-html="svg" @click="getCaptcha" />
+												</div>
+												<div class="layui-form-mid">
+													<span style="color: #c00;">{{ errors[0] }}</span>
+												</div>
+											</div>
+										</validation-provider>
+									</div>
+									<div class="layui-form-item">
+										<button
+											class="layui-btn"
+											lay-filter="*"
+											lay-submit
+											type="button"
+											@click="validate().then(submit)"
+										>
+											立即登录
+										</button>
+										<span style="padding-left:20px;">
+											<router-link :to="{ name: 'forget' }"
+												>忘记密码？</router-link
+											>
+										</span>
+									</div>
+									<div class="layui-form-item fly-form-app">
+										<span>或者使用社交账号登入</span>
+										<a
+											href
+											onclick="layer.msg('正在通过QQ登入', {icon:16, shade: 0.1, time:0})"
+											class="iconfont icon-qq"
+											title="QQ登入"
+										></a>
+										<a
+											href
+											onclick="layer.msg('正在通过微博登入', {icon:16, shade: 0.1, time:0})"
+											class="iconfont icon-weibo"
+											title="微博登入"
+										></a>
+									</div>
+								</form>
+							</div>
 						</div>
-					</div>
+					</validation-observer>
 				</div>
 			</div>
 		</div>
@@ -127,12 +137,13 @@
 <script>
 import { getCode, login } from "../api/login";
 import uuid from "uuid/dist/v4";
-import { ValidationProvider } from "vee-validate";
+import { ValidationProvider, ValidationObserver } from "vee-validate";
 
 export default {
 	name: "Login",
 	components: {
 		ValidationProvider,
+		ValidationObserver,
 	},
 	data() {
 		return {
@@ -162,8 +173,11 @@ export default {
 				this.svg = res.data;
 			});
 		},
-		submit() {
-			console.log(this.submit);
+		async submit() {
+			const isValid = await this.$refs.observer.validate();
+			if (!isValid) {
+				return;
+			}
 			login({
 				username: this.userName,
 				password: this.password,
